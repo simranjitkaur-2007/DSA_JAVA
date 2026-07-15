e2bbbdbda29450b87b9506078490c38d8be39bed
Simranjit Kaur <simran785917@gmail.com>
2026-07-15 22:31:31 +0530

    Correct switch statement and import in Basics.java
    Fix syntax errors in switch statement and import.

--- Basics.java/switch.java
+++ Basics.java/switch.java
@@ -1,1 +1,11 @@
-
+import java.util.System;
+public class week{
+  public static void main(String[] args){
+    Scanner in=new Scanner;
+    int day = in.nextInt();
+    switch(day){
+      case 1,2,3,4,5 -> System.out.println("week day");
+      case 6,7 -> System.out.println("weekend");
+    }
+  }
+}
